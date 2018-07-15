9bbf803428f948722e2eab005d8facd11004addd
ManifoldFR <wilson.jallet@gmail.com>
2018-07-15 13:20:23 +0200

    hawkes example graph

--- examples/hawkes_exp.rs
+++ examples/hawkes_exp.rs
@@ -1,13 +1,16 @@
 extern crate pointprocesses;
 extern crate plotlib;
+extern crate svg;
 extern crate serde_json;
 
 use std::fs;
+use std::path::Path;
+
+use svg::Node;
 
 use plotlib::style::{Point, Marker, Line};
 use plotlib::view;
-use plotlib::page;
-//use plotlib::function;
+use plotlib::view::View;
 use plotlib::line;
 use plotlib::scatter;
 use plotlib::scatter::Scatter;
@@ -17,10 +20,10 @@ use pointprocesses::hawkes_exponential;
 
 fn main() {
     
-    let tmax = 20.0;
-    let alpha = 0.5;
-    let beta = 0.8;
-    let lambda0 = 1.0;
+    let tmax = 90.0;
+    let alpha = 0.8;
+    let beta = 1.0;
+    let lambda0 = 0.9;
 
     let events: Vec<Event> = hawkes_exponential(tmax, alpha, beta, lambda0);
 
@@ -45,7 +48,7 @@ fn main() {
         result + lambda0
     };
 
-    let samples = 1000;
+    let samples = 3000;
     let times = (0..samples).map(|i| {
         i as f64*tmax/samples as f64
     });
@@ -59,35 +62,30 @@ fn main() {
 
     let intens_plot = line::Line::new(&intens_data)
         .style(line::Style::new()
-            .width(1.5)
+            .width(1.2)
             .colour("#4C36EB")
         );
-
-    /*
-    let intens_plot = function::Function::new(
-        |t| intensity_func(&events, t), 0.0, tmax)
-        .style(function::Style::new()
-            .width(1.5)
-            .colour("#4C36EB"));
-    */
-
+    
     let ev_tupl: Vec<(f64,f64)> = events.into_iter()
         .map(|e: Event| {
             (e.timestamp(), e.intensity())
         }).collect();
     
     let sc = Scatter::from_slice(&ev_tupl)
-        .style(scatter::Style::new().size(2.0)
-            .colour("#E0A536")
-            .marker(Marker::Cross)
+        .style(scatter::Style::new().size(1.2)
+            .colour("#FF0000")
+            .marker(Marker::Circle)
         );
 
     let v = view::ContinuousView::new()
-        .x_label("Temps t")
-        .y_label("Intensité λ(t)")
+        .x_label("Time t")
+        .y_label("Intensity λ(t)")
         .add(&intens_plot)
         .add(&sc);
 
     fs::create_dir("examples/images").unwrap_or_default();
-    page::Page::single(&v).save("examples/images/hawkes_exp.svg");
+    let mut document = svg::Document::new().set("viewBox", (0,0,900,400));
+    let savepath = Path::new("examples/images/hawkes_exp.svg");
+    document.append(v.to_svg(850., 350.).set("transform", format!("translate({}, {})", 40, 360)));
+    svg::save(savepath, &document).unwrap();
 }
