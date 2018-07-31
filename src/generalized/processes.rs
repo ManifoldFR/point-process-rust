d9663489bd230ed371cde9a2266d0def79a73ab5
ManifoldFR <wilson.jallet@gmail.com>
2018-07-31 23:56:10 +0200

    use filter_map when generating events, use slice

--- src/generalized/processes.rs
+++ src/generalized/processes.rs
@@ -4,98 +4,94 @@ use ndarray::stack;
 use ndarray::prelude::*;
 
 use rand::thread_rng;
-use rand::distributions::Uniform;
+use rand::distributions::Range;
 use rand::distributions::Poisson;
 use rand::prelude::*;
 
-use std::sync::Arc;
-use rayon::prelude::*;
+static XORSHIFT_ERR: &str = "Unable to create XorShift rng from thread local rng";
 
 /// A higher-dimensional homogeneous Poisson process.
 pub fn poisson_process<T>(lambda: f64, domain: &T) -> Array2<f64> 
-    where T: Set {
+    where T: Set
+{
+    let ref mut rng = thread_rng();
     let bounds = domain.bounding_box();
-    let mut area = 1.0;
 
     // dimension of space
     let d = bounds.shape()[1];
-
-    for i in 0..d {
-        area *= bounds[[1,i]] - bounds[[0,i]];
-    }
+    let area = (0..d).fold(1.0, |area, i| {
+        area * (bounds[[1,i]] - bounds[[0,i]])
+    });
 
     // get number of events to generate
     // events outside of the set will be rejected
-    let ref mut rng = thread_rng();
     let num_events = Poisson::new(lambda*area).sample(rng) as usize;
 
-    let mut res = unsafe {
-        Array::uninitialized((1,d))
-    };
-    
-    for _ in 0..num_events {
+    let mut srng = rand::rngs::SmallRng::from_rng(rng).expect(XORSHIFT_ERR);
+
+    let events: Vec<Array2<f64>> = (0..num_events).filter_map(|_| {
         // generate a point inside the bounding box
-        let mut ev = Array::zeros((d,));
+        let mut ev: Array1<f64> = Array::zeros((d,));
 
         for i in 0..d {
-            ev[i] = rng.sample(Uniform::new(bounds[[0,i]], bounds[[1,i]]));
+            ev[i] = srng.sample(Range::new(bounds[[0,i]], bounds[[1,i]]));
         }
 
         // if it's in, then keep it
         if domain.contains(&ev) {
-            res = stack(
-                Axis(0), 
-                &[res.view(), ev.into_shape((1,d)).unwrap().view()]
-                ).unwrap();
+            Some(ev.into_shape((1,d)).unwrap())
+        } else {
+            None
         }
-    }
+    }).collect();
 
-    res
+    let events_ref: Vec<ArrayView2<f64>> = events.iter().map(|ev| {
+        ev.view()
+    }).collect();
+
+    stack(Axis(0), events_ref.as_slice()).unwrap()
 }
 
 /// Poisson process on a d-dimensional region with variable intensity, using a rejection sampling algorithm.
 pub fn variable_poisson<F, T>(lambda: F, max_lambda: f64, domain: &T) -> Array2<f64>
-where F: Fn(&Array1<f64>) -> f64 + Sync + Send,
-      T: Set
+    where F: Fn(&Array1<f64>) -> f64 + Sync + Send,
+          T: Set
 {
     let bounds = domain.bounding_box();
-    let mut area = 1.0;
 
     let d = bounds.shape()[1];
-
-    for i in 0..d {
-        area *= bounds[[1,i]] - bounds[[0,i]];
-    }
+    let area = (0..d).fold(1.0, |area, i| {
+        area * (bounds[[1,i]] - bounds[[0,i]])
+    });
 
     // get number of events to generate
     // events outside of the set will be rejected
     let ref mut rng = thread_rng();
     let num_events = Poisson::new(max_lambda*area).sample(rng) as usize;
 
-    let mut res = unsafe {
-        Array::uninitialized((1,d))
-    };
-    
-    for _ in 0..num_events {
+    let mut srng = rand::rngs::SmallRng::from_rng(rng).expect(XORSHIFT_ERR);
+
+    let events: Vec<Array2<f64>> = (0..num_events).filter_map(|_| {
         // generate a point inside the bounding box
-        let mut ev = Array::zeros((d,));
+        let mut ev: Array1<f64> = Array::zeros((d,));
         let intens = max_lambda*random::<f64>();
 
         for i in 0..d {
-            ev[i] = rng.sample(Uniform::new(bounds[[0,i]], bounds[[1,i]]));
+            ev[i] = srng.sample(Range::new(bounds[[0,i]], bounds[[1,i]]));
         }
 
-        // if the point lies in the domain and the simulated intensity
-        // fits, then add it.
+        // if it's in, then keep it
         if domain.contains(&ev) && intens < lambda(&ev) {
-            res = stack![
-                Axis(0), 
-                res.view(),
-                ev.view().into_shape((1,d)).unwrap()
-            ];
+            Some(ev.into_shape((1,d)).unwrap())
+        } else {
+            None
         }
-    }
+    }).collect();
 
-    res
+    let events_ref: Vec<ArrayView2<f64>> = events.iter().map(|ev| {
+        ev.view()
+    }).collect();
+
+    stack(Axis(0), &events_ref).unwrap()
 }
 
